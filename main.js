96c820ef2d3f92e3d62174389a3600abbd553fe9
rust_lang_lover <rust_lang_lover@proton.me>
2024-09-16 15:58:17 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = process.env.DEPLOY_PORT || 8004;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+const directoryPath = path.join(__dirname, 'public/images');
+
+app.get('/image-plugin/extract_all/', (req, res) => {
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+            return res.status(500).json({ error: 'Unable to scan directory' });
+        }
+        else {
+            let returnable_array = [];
+            files.forEach((file_path) => {
+                returnable_array.push(`https://new-api.space/images/${file_path}`)
+            })
+            res.json({
+                extracted : returnable_array
+            });
+        }
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Images Plugin Express Api is running at http://localhost:${port}`);
+});
